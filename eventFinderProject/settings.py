6159926bd41bdeaf29710fe8e40c13ccec787721
Hayley van Waas <hayleyavw@gmail.com>
2019-09-15 10:03:51 +0800

    moved static files, WIP api specs file

--- eventFinderProject/settings.py
+++ eventFinderProject/settings.py
@@ -124,6 +124,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 AUTH_USER_MODEL = 'users.CustomUser'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
